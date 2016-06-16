78982c85cac9dce7a34f0068c43dcf14473063ef
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2016-06-17 00:15:28 +0530

    adding dummy users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -19,20 +19,46 @@ class UserSeeder extends Seeder
         $user5 = new \App\User();
         $user6 = new \App\User();
 
+        $user->given_name = "Anuradha";
+        $user->family_name = "Wickramarachchi";
+        $user->name = "Anuradha Wickramarachchi";
+        $user->password = bcrypt("732115526464-it7hknll4or0fmhore01ud96ufkd9u2d.apps.googleusercontent.com");
+        $user->sub = "12334";
+        $user->picture = "https://lh4.googleusercontent.com/-t_3z1ySLNNU/AAAAAAAAAAI/AAAAAAAACts/noKs8BL3vug/s96-c/photo.jpg";
+        $user->admin = 1;
         $user->email = "sanjeewa@live.com";
+
+        $user1->given_name = "Robinson";
+        $user1->family_name = "Crusoe";
+        $user1->name = "Robinson Crusoe";
+        $user1->password = bcrypt("732115526464-it7hknll4or0fmhore01ud96ufkd9u2d.apps.googleusercontent.com");
+        $user1->sub = "1233asd4";
+        $user1->picture = "https://lh4.googleusercontent.com/-t_3z1ySLNNU/AAAAAAAAAAI/AAAAAAAACts/noKs8BL3vug/s96-c/photo.jpg";
+        $user1->admin = 1;
         $user1->email = "robinson@gmail.com";
+
+        $user2->given_name = "Ravidu";
+        $user2->family_name = "Mallawa Arachchi";
+        $user2->name = "Ravidu Mallawa Arachchi";
+        $user2->password = bcrypt("732115526464-it7hknll4or0fmhore01ud96ufkd9u2d.apps.googleusercontent.com");
+        $user2->sub = "1233114";
+        $user2->picture = "https://lh4.googleusercontent.com/-t_3z1ySLNNU/AAAAAAAAAAI/AAAAAAAACts/noKs8BL3vug/s96-c/photo.jpg";
+        $user2->admin = 1;
         $user2->email = "ravidu@protonmail.com";
+
+        $user3->given_name = "Dulaj";
+        $user3->family_name = "Atapattu";
+        $user3->name = "Dualaj Atapattu";
+        $user3->password = bcrypt("732115526464-it7hknll4or0fmhore01ud96ufkd9u2d.apps.googleusercontent.com");
+        $user3->sub = "1qq2334";
+        $user3->picture = "https://lh4.googleusercontent.com/-t_3z1ySLNNU/AAAAAAAAAAI/AAAAAAAACts/noKs8BL3vug/s96-c/photo.jpg";
+        $user3->admin = 1;
         $user3->email = "dulaj@ymail.com";
-        $user4->email = "pamoda@yahoo.com";
-        $user5->email = "chamal@outlook.com";
-        $user6->email = "pasindu@hotmail.com";
+
 
         $user->save();
         $user1->save();
         $user2->save();
         $user3->save();
-        $user4->save();
-        $user5->save();
-        $user6->save();
     }
 }
